7296d92202eacd9fbfba244dd78e94e83c5c29e7
Daniel Ufeli <danielufeli@yahoo.com>
2025-03-06 15:52:42 +0100

    deployment

--- src/data-source.ts
+++ src/data-source.ts
@@ -16,7 +16,7 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  synchronize: false,
+  synchronize: true,
   logging: true,
   ssl:
   process.env.DB_USE_SSL === "true"
