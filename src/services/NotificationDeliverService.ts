2aa44ea3bb32bcd19303b8829c222997da1a66cf
Daniel Ufeli <danielufeli@yahoo.com>
2025-05-02 21:10:33 +0100

    Set Secure to false

--- src/services/NotificationDeliverService.ts
+++ src/services/NotificationDeliverService.ts
@@ -26,12 +26,14 @@ export class NotificationDeliveryService {
     this.emailTransporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: parseInt(process.env.SMTP_PORT || "587"),
-      secure: process.env.SMTP_SECURE === "true",
+      secure: false,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
       },
-      tls: { rejectUnauthorized: false },
+      tls: {
+        rejectUnauthorized: true,
+      },
     });
 
     this.emailTransporter.verify((error, success) => {
